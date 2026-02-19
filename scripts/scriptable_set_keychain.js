37ed8e1e92743f6ba1710c5ba58023dc0a0b20fb
Allard <allardqjy@gmail.com>
2026-02-19 11:42:52 +0800

    Remove whitespace

--- scripts/scriptable_set_keychain.js
+++ scripts/scriptable_set_keychain.js
@@ -1,4 +1,4 @@
-    // one-time: set your values
-    Keychain.set("ELASTIC_API_URL", "");
-    Keychain.set("ELASTIC_API_KEY", "");
-    console.log("Saved keys");
+// one-time: set your values
+Keychain.set("ELASTIC_API_URL", "");
+Keychain.set("ELASTIC_API_KEY", "");
+console.log("Saved keys");
